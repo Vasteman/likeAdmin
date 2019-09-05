003028d4a0c1d9add690f6722aea71b7dc06b391
Mikhail <mihail.yav@yandex.ru>
2019-09-05 22:00:32 +0300

    install antd, set eslint

--- like_admin/src/App.js
+++ like_admin/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Button, Icon } from 'antd'
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
+      <Button type='danger'>Primary</Button>
+      <Icon type="play-circle" />
+    {/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +23,10 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div> */}
+    </Fragment>
+  )
+
 }
 
-export default App;
+export default App
