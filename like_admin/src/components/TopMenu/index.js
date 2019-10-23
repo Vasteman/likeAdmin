2babfa2761831fba7d4c5a9ed86acbc27018ab56
Vasteman <43703155+Vasteman@users.noreply.github.com>
2019-10-23 12:25:33 +0300

    Update index.js

--- like_admin/src/components/TopMenu/index.js
+++ like_admin/src/components/TopMenu/index.js
@@ -1,28 +1,28 @@
 import React, { Component, Fragment } from 'react'
-import { Route,Redirect, Link} from 'react-router-dom'
+import { Route,Redirect, Link} from 'react-router-dom' // spacing
 import styled from 'styled-components'
 
 
-const MenuItems = [
-  { title: 'Домой', path: '/start'},
-  { title: 'Типы лайков', path: '/likeAdmin/typesOfLikes'},
-  { title: 'Релизы', path: '/likeAdmin/releases'},
-  { title: 'Features', path: '/likeAdmin/features'}
-  ]
+const MenuItems = [ // menuItems - js-object, а не React.Component, пишется с маленькой
+  { title: 'Домой', path: '/start'}, // space перед "}"
+  { title: 'Типы лайков', path: '/likeAdmin/typesOfLikes'}, // space перед "}"
+  { title: 'Релизы', path: '/likeAdmin/releases'}, // space перед "}"
+  { title: 'Features', path: '/likeAdmin/features'} // space перед "}"
+  ] // минус tab
 
 class TopMenu extends Component{
   render() {
-    console.log('1111');
+    console.log('1111'); // минус консоль
     return (
-      <Fragment>
+      <Fragment> // MenuWrapper - один компонент в return, в чем необходимость Fragment?
         <MenuWrapper>
-          {MenuItems.map(item => {
+          {MenuItems.map(item => { // menuItems - js-object, а не React.Component, пишется с маленькой
             return(
-              <StyledLink to={item.path}>
+              <StyledLink to={item.path}> // по хорошему надо прописать key, и желательно не index
                 <MenuItemName>{item.title}</MenuItemName>
                </StyledLink>
             )
-          })
+          }) // что-то не то с переносами скобок
           }
         </MenuWrapper>
       </Fragment>
@@ -45,12 +45,12 @@ const StyledLink = styled(Link)`
   display: block;
   border-right: 1px solid #fff;
   padding: 15px 10px 0 10px;
-  border-right: 1px solid #fff;
+  border-right: 1px solid #fff; // double border-right
   height: 60px;
   width: 100px;
   text-align: center;
 
-  :hover{
+  :hover{ // empty :hover
 
   }
 `
